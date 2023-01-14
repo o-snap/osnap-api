e2e5b230489c3e2ba4bbf41eff0b376c6067dd25
cannotilever <cannotilever@protonmail.com>
2023-01-14 13:27:31 -0500

    add handler functions

--- src/main.rs
+++ src/main.rs
@@ -3,16 +3,26 @@ use argon2::{password_hash::PasswordHasher,Argon2};
 use rocket_db_pools::{sqlx::{self,Row}, Database, Connection};
 use rand_core::{RngCore, OsRng};
 use serde::{Deserialize, Serialize};
-use serde_json::{Result};
 mod structures;
+use rocket::serde::{json::Json};
 
 #[get("/")]
 fn index() -> &'static str {
     "O-Snap API server v0.1.0-goathack"
 }
 
+#[post("/profile", format="json", data = "<request>")]
+fn profile(request: Json<structures::ProfRequest<'_>>) -> Json<structures::Profile>{
+	
+}
+
 
 #[launch]
 fn rocket() -> _ {
     rocket::build().mount("/", routes![index])
+}
+
+fn checkauth<'r>(key: String) -> Result<&'r str, &'r str>{
+	
+	Err("unauthorized")
 }
